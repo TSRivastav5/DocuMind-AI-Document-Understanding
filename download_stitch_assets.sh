10efecfcdc8651d15c1fe8a907883eaf4f506987
Trishant Srivastav <trishantsrivastav@Trishants-MacBook.local>
2026-05-14 14:07:04 +0530

    clean up unused design mockups and scripts

--- download_stitch_assets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Downloading DocuMind Next-Gen AI Dashboard Screenshot..."
-curl -L "https://lh3.googleusercontent.com/aida/ADBb0uhXkoYSX7pdD4TkxqT2-Te9EXufI7cyyiyzZ1JuaW9oTBCHu68IFNyw_U4lRYDmFUf-x3bD-VgvatVFEXL85rlUAvPQYxAbh9jCHETMHyIB6Q-Hfu-UuITWsWaU--Wy3m-lMywXwRWPRCSnR3K4his_A1duWty8_558boDjFIyugIVqF9gKHgmYCEnFiwjvKP3WwY5VVgsSHpBFU7sZ6yzxFXfHMjDqwMCzVj_obU0hDyc2hoQdSGc20Q" -o documind_dashboard.png
-
-echo "Downloading DocuMind Next-Gen AI Dashboard HTML Code..."
-curl -L "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzUyNDJmYTk1MGUxMzQ4YTRiY2IxZmRkYTkxYTQ4NWVjEgsSBxDE9v3TgQsYAZIBIwoKcHJvamVjdF9pZBIVQhM1Mjc5NDM3Mjg3OTE4NDg1NDU2&filename=&opi=89354086" -o documind_dashboard.html
-
-echo "Download completed."
